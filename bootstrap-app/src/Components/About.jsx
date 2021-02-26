871a43a6885f8c47764329ded169cd7c5c2dba59
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-02-26 07:54:47 +0100

    debug About

--- bootstrap-app/src/Components/About.jsx
+++ bootstrap-app/src/Components/About.jsx
@@ -25,6 +25,7 @@ export default function About() {
             " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!",
       },
    ];
+   let count = 0;
    return (
       <section className="page-section" id="about">
          <div className="container">
@@ -37,8 +38,9 @@ export default function About() {
 
             <ul className="timeline">
                {data.map((item) => (
-                   
-                  <li>
+
+                  <li  className={count % 2 === 0 ? "timeline-inverted":""} > 
+
                      <div className="timeline-image">
                         <img
                            className="rounded-circle img-fluid"
@@ -55,82 +57,13 @@ export default function About() {
                            <p className="text-muted">{item.description}</p>
                         </div>
                      </div>
-                  </li>
+                    <p style={{'display':'none'}}>{count +=1}</p>
+                  </li >
                ))}
 
-               {/* <li className="timeline-inverted">
-                  <div className="timeline-image">
-                     <img
-                        className="rounded-circle img-fluid"
-                        src="assets/img/about/2.jpg"
-                        alt=""
-                     />
-                  </div>
-                  <div className="timeline-panel">
-                     <div className="timeline-heading">
-                        <h4>March 2011</h4>
-                        <h4 className="subheading">An Agency is Born</h4>
-                     </div>
-                     <div className="timeline-body">
-                        <p className="text-muted">
-                           Lorem ipsum dolor sit amet, consectetur adipisicing
-                           elit. Sunt ut voluptatum eius sapiente, totam
-                           reiciendis temporibus qui quibusdam, recusandae sit
-                           vero unde, sed, incidunt et ea quo dolore laudantium
-                           consectetur!
-                        </p>
-                     </div>
-                  </div>
-               </li>
-
-
-               <li>
-                  <div className="timeline-image">
-                     <img className="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="" />
-                  </div>
-                  <div className="timeline-panel">
-                     <div className="timeline-heading">
-                        <h4>December 2012</h4>
-                        <h4 className="subheading">
-                           Transition to Full Service
-                        </h4>
-                     </div>
-                     <div className="timeline-body">
-                        <p className="text-muted">
-                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam
-                           reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!
-                        </p>
-                     </div>
-                  </div>
-               </li>
-
                <li className="timeline-inverted">
                   <div className="timeline-image">
-                     <img className="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="" />
-                  </div>
-                  <div className="timeline-panel">
-                     <div className="timeline-heading">
-                        <h4>July 2014</h4>
-                        <h4 className="subheading">Phase Two Expansion</h4>
-                     </div>
-                     <div className="timeline-body">
-                        <p className="text-muted">
-                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!
-                        </p>
-                     </div>
-                  </div>
-               </li>
- */}
-
-               <li className="timeline-inverted">
-                  <div className="timeline-image">
-                     <h4>
-                        Be Part
-                        <br />
-                        Of Our
-                        <br />
-                        Story!
-                     </h4>
+                     <h4>Be Part<br />Of Our<br />Story! </h4>
                   </div>
                </li>
             </ul>
