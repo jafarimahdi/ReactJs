c29ea5d9e317b1fcebaac51122a0ed05d514470f
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-03-19 08:53:59 +0100

    buttonComponent

--- web-site/src/component/Button.js
+++ web-site/src/component/Button.js
@@ -2,12 +2,30 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 
+const STYLES =['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
 export const Button = ({
-   children,
-   type,
-   onclick,
-   buttonStyle,
-   buttonSize,
+    children,
+    type,
+    buttonStyle,
+    onClick,
+    buttonSize,
 }) => {
-    
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonSize: STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize)?buttonSize: SIZES[0];
+
+    return(
+        <Link to='/signup' className="btn-mobile">
+            <button
+                className=`btn ${checkButtonStyle} ${checkButtonSize}`
+                onClick={onClick}
+                type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    )
 };
--- web-site/src/component/Navbar.js
+++ web-site/src/component/Navbar.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
 
 function Navbar() {
 const [click, setClick] = useState(false);
+const [button, setButton]= useState(true);
 
 const handelClick = () => setClick(!click);
-const closeMobileMenu = () => setClick(false)
+const closeMobileMenu = () => setClick(false);
+
+const showButton =()=>{
+   if(window.innerWidth <= 960){
+      setButton(false)
+   }
+   setButton(true);
+}
 
 return (
    <>
@@ -32,6 +41,7 @@ return (
                   <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}> Sign Up </Link>
                </li>
             </ul>
+            {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
          </div>
       </nav>
    </>
