8ee6daef5e2b98ac9b51733d9428c2bff6c0a2f8
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-03-27 11:17:27 +0100

    cards

--- web-site/src/component/Cards.js
+++ web-site/src/component/Cards.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import CardItem from './CardItem';
+import img2 from '../images/img-2.jpg';
+import './Cards.css';
 
 function Cards() {
     return (
@@ -8,7 +10,9 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                    <CardItem />
+                    <CardItem 
+                        src={img2}
+                    />
                     </ul>
                 </div>
             </div>
