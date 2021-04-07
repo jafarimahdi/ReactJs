d636478ec28745b597067b76c9f7e91d805d8790
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-04-07 09:07:56 +0200

    paths

--- web-site/src/App.js
+++ web-site/src/App.js
@@ -1,8 +1,11 @@
-import React from "react";  
+import React from "react";
 import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from './component/pages/Home';
+import Home from "./component/pages/Home";
+import Product from "./component/pages/Product";
+import Services from "./component/pages/Services";
+import SignUp from "./component/pages/SignUp";
 
 function App() {
   return (
@@ -10,12 +13,14 @@ function App() {
         <Router>
             <Navbar />
             <Switch>
-              <Route path="/" exact component={Home} />
+              <Route path="/home" exact component={Home} />
+              <Route path="/Service" exact component={Services} />
+              <Route path="/product" exact component={Product} />
+              <Route path="/signUp" exact component={SignUp} />
             </Switch>
         </Router>
       </>
   );
-
 }
 
 export default App;
