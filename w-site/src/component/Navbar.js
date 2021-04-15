63530e7f25da2f4bc963ad2dc35b6324c169f4d9
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-04-15 06:43:21 +0200

    w-site

--- w-site/src/component/Navbar.js
+++ w-site/src/component/Navbar.js
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {Button} from './Button';
+import './Navbar.css';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton =() => {
+        if(window.innerWidth <= 960){
+            setButton(false)
+        } else {
+            setButton(true)            
+        }
+    };
+
+    window.addEventListener('resize',showButton);
     return (
         <>
             <nav className="navbar">
@@ -9,6 +26,37 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         MJ <i className="fas fa-spider" />
                     </Link>
+
+                    <div className="menu-icon" onClick={handleClick}> 
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>
+
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/"  className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/services"  className="nav-links" onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/product"  className="nav-links" onClick={closeMobileMenu}>
+                                Product
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/sign-up"  className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Sign Up**
+                            </Link>
+                        </li>
+                    </ul>
+
+
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+
                 </div>
             </nav>
         </>
@@ -16,3 +64,6 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+// youtube : 38.27
