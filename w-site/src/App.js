d73438f7982bfa0c57a5bc81d1cb3e45d739f255
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-04-17 10:38:48 +0200

    refactoring root tree

--- w-site/src/App.js
+++ w-site/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./component/Navbar";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './component/pages/Home';
+import Home from './pages/Home';
 
 function App() {
   return (
