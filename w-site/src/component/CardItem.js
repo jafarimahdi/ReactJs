d9efdd4bb665f3745392778d5e7d64c9ada88036
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-04-19 08:39:39 +0200

    add cardComponent

--- w-site/src/component/CardItem.js
+++ w-site/src/component/CardItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function CardItem() {
     return (
@@ -7,8 +7,12 @@ function CardItem() {
             <li className="cards__item">
                 <Link className="cards__item__link">
                     <figure className="cards__item__pic-wrap">
-                        
+                        <img src="/" alt="Travel Image" className="cards__item__img"/>
                     </figure>
+
+                    <div className="cards__item__info">
+                        <h5 className="cards__item__text"> heoooo</h5>
+                    </div>
                 </Link>
             </li>
         </>
--- w-site/src/pages/Home.js
+++ w-site/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../App.css';
 import HeroSection from '../component/HeroSection';
+import Cards from '../component/Cards';
 
 function Home(){
     return (
         <>
             <HeroSection/>
+            <Cards/>
         </>
     )
 }
