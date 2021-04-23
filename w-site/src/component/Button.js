6bc0a8b31b831b2f103e4072fc0d41a98822f843
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-04-23 06:44:59 +0200

    build & button

--- w-site/src/component/Button.js
+++ w-site/src/component/Button.js
@@ -15,7 +15,7 @@ export const Button = ({children, type, onClick, buttonStyle, buttonSize})=>{
         return(
             <Link to='/sign-up' className='btn-mobile'>
                 <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
-                    {children} heool
+                    {children}
                 </button>
             </Link>
         )
