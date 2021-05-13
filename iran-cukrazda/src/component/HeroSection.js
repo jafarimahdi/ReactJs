758969b4f684fcf51e08f5b91bada4ec01be6563
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-05-13 15:38:48 +0200

    heroSection

--- iran-cukrazda/src/component/HeroSection.js
+++ iran-cukrazda/src/component/HeroSection.js
@@ -5,8 +5,7 @@ import "./HeroSection.css";
 
 function HeroSection() {
     return (
-        <div className="hero-container" style={{ backgroundImage: "linear-gradient(80deg, rgba(0,0,0,.8), transparent), url(img-2/1.jpg)"}}>
-            {/* <video src="/Videos/video-1.mp4" autoPlay loop muted /> */}
+        <div className="hero-container" style={{ backgroundImage: "linear-gradient(20deg, rgba(0,0,0,.8), transparent), url(img-2/1.jpg)"}}>
             <h1>Iran Cukrazda</h1>
             <p>DESERT SHOP</p>
         </div>
