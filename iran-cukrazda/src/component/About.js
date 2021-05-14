302ea4a9d6729f4f5f5f5439821dc4e97de6ecab
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-05-14 09:04:37 +0200

    aboutComponent

---
+++ iran-cukrazda/src/component/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export function About(){
+    return(
+        <>
+        <div className="lineBreak">
+            <img src="./img-2/2-sv.png" alt="" />
+        <h3>About Us</h3>
+        </div>
+        <div className="detail">
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <p>Feri Bacsi</p>
+            <p>Freydon learned pastry in his family business as traditions way and how to prepare all the product by Hand</p>
+        </div>
+        
+        
+        </>
+    )
+}
--- iran-cukrazda/src/pages/Home.js
+++ iran-cukrazda/src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../App.css';
 import HeroSection from '../component/HeroSection';
 import Cards from '../component/Cards';
+import { About } from '../component/About';
 
 function Home(){
     return (
         <>
             <HeroSection/>
+            <About/>
             <Cards/>
         </>
     )
