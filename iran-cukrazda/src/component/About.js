a310a631ca2763ad304f952f099cbce9c61a1e21
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-05-18 22:11:34 +0200

    about component

--- iran-cukrazda/src/component/About.js
+++ iran-cukrazda/src/component/About.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import './About.scss';
+import React, { Component } from "react";
+import "./About.scss";
 
-export function About(){
-
-
-    
-    return(
+export function About() {
+    return (
         <>
-        <div className="lineBreak">
-            <img className="line-image" src="./img-2/2-sv.png" alt="" />
-        <h3>About Us</h3>
-        </div>
-        <div className="detail">
-            <img src="" alt="" />
-            <p>Feri Bacsi</p>
-            <img src="" alt="" />
-            <p>Freydon learned pastry in his family business as traditions way and how to prepare all the product by Hand</p>
-        </div>
-        
-        
+            <div className="lineBreak">
+                <img className="line-image" src="./img-2/2-sv.png" alt="" />
+                <h1>About Us</h1>
+            </div>
+
+            <div className="frei">
+                <img src="./img-2/6.jpg" alt="" />
+
+                <p>Iran Cukarzda is a place, where you can find traditional and handmade Persian Sweets with unique test of the ‘Saffron ,Rose Water, Honey, Pistachios’.</p>
+            </div>
+            <div className="freiStory">
+                <img src="./img-2/15.jpg" alt="" />
+                <p>
+                Mr. Fereydon was working in his family business in Iran, learning the craft of local pastry-making, after being forced to leave Iran, and Working for 20 years in Norway, he is now among those adept artisans who have exported their know-how of a traditional trade to the Hungarian Capital .. 
+                </p>
+            </div>
         </>
-    )
+    );
 }
