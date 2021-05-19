8f48ae3672ac272e898126c0a9900473d5340960
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-05-19 08:19:02 +0200

    product component

--- iran-cukrazda/src/component/Cards.js
+++ iran-cukrazda/src/component/Cards.js
@@ -1,50 +1,58 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.scss';
+import { Link } from 'react-router-dom';
 
 function Cards() {
     return (
         <div className='cards'>
-            <h1>Check out these EPIC Destinations!</h1>
+            <img src="./img-2/4-sv.png" className="headSvg" alt="" />
+            <h1>Our Product</h1>
 
             <div className="cards__container">
                 <div className="cards__wrapper">
+
                     <ul className="cards__items">
                         <CardItem
-                            src="images/img-1.jpg"
+                            src="img-2/5.jpg"
                             text='Explore the hidden waterfall deep inside tha Amazon jangle'
-                            label='Adventure'
-                            path='/services'
+                            label='ZABAN'
+                            path='/product'
                         />
                         <CardItem
-                            src="images/img-2.jpg"
+                            src="img-2/7.jpg"
                             text='Travel through to the Island of Bali in a Private Crouse'
                             label='Adventure'
-                            path='/services'
+                            path='/product'
                         />
                     </ul>
+
                     <ul className="cards__items">
                         <CardItem
-                            src="images/img-3.jpg"
+                            src="img-2/8.jpg"
                             // src="videos/video-1.mp4"
                             text='Find peace in nature and free your soul'
                             label='Adventure'
-                            path='/services'
+                            path='/product'
                         />
                         <CardItem
-                            src="images/img-4.jpg"
+                            src="img-2/9.jpg"
                             text='Follow you favorite Team for the latest Match'
                             label='Adventure'
-                            path='/services'
+                            path='/product'
                         />
                         <CardItem
-                            src="images/img-5.jpg"
+                            src="img-2/12.jpg"
                             text='Visit to Capitals of the most Truistic place in the world'
                             label='Touristic Area'
-                            path='/services'
+                            path='/product'
                         />
                     </ul>
+
                 </div>
+                <Link to="/product" className="button-more">
+                <button>More</button>
+                </Link>
             </div>
         </div>
     )
