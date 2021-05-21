ef0f926a20659b9ece538e74b2ecc6f16a3ca85d
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-05-21 06:43:14 +0200

    refactoring

--- iran-cukrazda/src/component/About.js
+++ iran-cukrazda/src/component/About.js
@@ -4,7 +4,7 @@ import "./About.scss";
 export function About() {
     return (
         <>
-            <div className="lineBreak">
+            <div className="line-break">
                 <img className="line-image" src="./img-2/2-sv.png" alt="" />
                 <h1>About Us</h1>
             </div>
@@ -14,7 +14,7 @@ export function About() {
 
                 <p>Iran Cukarzda is a place, where you can find traditional and handmade Persian Sweets with unique test of the ‘Saffron ,Rose Water, Honey, Pistachios’.</p>
             </div>
-            <div className="freiStory">
+            <div className="frei-story">
                 <img src="./img-2/15.jpg" alt="" />
                 <p>
                 Mr. Fereydon was working in his family business in Iran, learning the craft of local pastry-making, after being forced to leave Iran, and Working for 20 years in Norway, he is now among those adept artisans who have exported their know-how of a traditional trade to the Hungarian Capital .. 
--- iran-cukrazda/src/component/Cards.js
+++ iran-cukrazda/src/component/Cards.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import CardItem from './CardItem'
+import Carditem from './Carditem'
 import './Cards.scss';
 import { Link } from 'react-router-dom';
 
 function Cards() {
     return (
         <div className='cards'>
-            <img src="./img-2/4-sv.png" className="headSvg" alt="" />
+            <img src="./img-2/4-sv.png" className="head-svg" alt="" />
             <h1>Our Product</h1>
 
             <div className="cards__container">
                 <div className="cards__wrapper">
 
                     <ul className="cards__items">
-                        <CardItem
+                        <Carditem
                             src="img-2/5.jpg"
                             text='Explore the hidden waterfall deep inside tha Amazon jangle'
                             label='ZABAN'
                             path='/product'
                         />
-                        <CardItem
+                        <Carditem
                             src="img-2/7.jpg"
                             text='Travel through to the Island of Bali in a Private Crouse'
                             label='Adventure'
@@ -28,20 +28,20 @@ function Cards() {
                     </ul>
 
                     <ul className="cards__items">
-                        <CardItem
+                        <Carditem
                             src="img-2/8.jpg"
                             // src="videos/video-1.mp4"
                             text='Find peace in nature and free your soul'
                             label='Adventure'
                             path='/product'
                         />
-                        <CardItem
+                        <Carditem
                             src="img-2/9.jpg"
                             text='Follow you favorite Team for the latest Match'
                             label='Adventure'
                             path='/product'
                         />
-                        <CardItem
+                        <Carditem
                             src="img-2/12.jpg"
                             text='Visit to Capitals of the most Truistic place in the world'
                             label='Touristic Area'
