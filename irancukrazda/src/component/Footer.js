7eafb7c69f247b03dc4295f119f777846800536c
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-06-07 09:01:34 +0200

    footer

--- irancukrazda/src/component/Footer.js
+++ irancukrazda/src/component/Footer.js
@@ -6,7 +6,6 @@ import "./Footer.scss";
 function Footer() {
     return (
         <div className="footer-container">
-
             <section className="footer-subscription">
                 <p className="footer-subscription-heading">
                     Join the Adventure newsletter to recive out best vacation
@@ -15,22 +14,19 @@ function Footer() {
                 <p className="footer-subscription-text">
                     You can unsubscribe any time
                 </p>
-                <div className="input-areas">
-                    <form action="">
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Your Email"
-                            className="footer-input"
-                        />
-                        <Button buttonStyle="btn--outline">Subscribe</Button>
-                    </form>
-                </div>
+
+                <form action="" className="input-areas">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your Email"
+                        className="footer-input"
+                    />
+                    <Button buttonStyle="btn--outline">Subscribe</Button>
+                </form>
             </section>
 
-
             <div className="footer-links">
-
                 <div className="footer-link-wrapper">
                     <div className="footer-link-items">
                         <h2>About Us</h2>
@@ -38,7 +34,6 @@ function Footer() {
                         <Link to="/">Testimonials</Link>
                         <Link to="/">Careers</Link>
                         <Link to="/">Investors</Link>
-                        
                     </div>
                     <div className="footer-link-items">
                         <h2>Contact Us</h2>
@@ -69,58 +64,61 @@ function Footer() {
             </div>
 
 
-            <section className="social-media">
                 <div className="social-media-wrap">
-                    <div className="footer-logo">
-                        <Link className="social-logo">
-                            Mj
-                            <i className="fas fa-spider" />
-                        </Link>
+
+                    <Link className="social-logo">
+                        Made With
+                        <i class="fab fa-gratipay"/>
+                        In Apr 2021 By MJ.
+                    </Link>
+
+
+                    <div className="social-icons">
+                    <Link
+                        className="social-icon-link facebook"
+                        to="/"
+                        target="_blank"
+                        aria-label="facebook"
+                    >
+                        <i className="fab fa-facebook-f" />
+                    </Link>
+
+                    <Link
+                        className="social-icon-link instagram"
+                        to="/"
+                        target="_blank"
+                        aria-label="Instagram"
+                    >
+                        <i className="fab fa-instagram" />
+                    </Link>
+                    <Link
+                        className="social-icon-link youtube"
+                        to="/"
+                        target="_blank"
+                        aria-label="youtube"
+                    >
+                        <i className="fab fa-youtube" />
+                    </Link>
+                    <Link
+                        className="social-icon-link twitter"
+                        to="/"
+                        target="_blank"
+                        aria-label="twitter"
+                    >
+                        <i className="fab fa-twitter" />
+                    </Link>
+                    <Link
+                        className="social-icon-link linkedin"
+                        to="/"
+                        target="_blank"
+                        aria-label="linkedin"
+                    >
+                        <i className="fab fa-linkedin" />
+                    </Link>
                     </div>
-                    <small className="website-rights">Apr 2021</small>
-                    <div className="social-icons"></div>
-                        <Link
-                            className="social-icon-link facebook"
-                            to="/"
-                            target="_blank"
-                            aria-label="facebook"
-                        >
-                            <i className='fab fa-facebook-f'/>
-                        </Link>
-                        <Link
-                            className="social-icon-link instagram"
-                            to="/"
-                            target="_blank"
-                            aria-label="Instagram"
-                        >
-                            <i className='fab fa-instagram'/>
-                        </Link>
-                        <Link
-                            className="social-icon-link youtube"
-                            to="/"
-                            target="_blank"
-                            aria-label="youtube"
-                        >
-                            <i className='fab fa-youtube'/>
-                        </Link>
-                        <Link
-                            className="social-icon-link twitter"
-                            to="/"
-                            target="_blank"
-                            aria-label="twitter"
-                        >
-                            <i className='fab fa-twitter'/>
-                        </Link>
-                        <Link
-                            className="social-icon-link linkedin"
-                            to="/"
-                            target="_blank"
-                            aria-label="linkedin"
-                        >
-                            <i className='fab fa-linkedin'/>
-                        </Link>
-                    </div>
-            </section>
+
+                </div>
+            
         </div>
     );
 }
