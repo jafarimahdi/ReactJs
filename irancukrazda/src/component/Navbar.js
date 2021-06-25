960f320541536bbc6bb92e373c5c5db2eaf7fd4f
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-06-25 06:52:36 +0200

    refactoring navBar

--- irancukrazda/src/component/Navbar.js
+++ irancukrazda/src/component/Navbar.js
@@ -4,24 +4,9 @@ import "./Navbar.scss";
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-
-    useEffect(() => {
-        showButton();
-    }, []);
-
-    // window.addEventListener('resize',showButton);
     return (
         <>
             <nav className="navbar">
