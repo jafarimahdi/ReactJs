7ac5366ad9ebe33d131a5caa970388dd240603be
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-07-03 08:06:34 +0200

    review

--- kalunba/src/Components/Footer.jsx
+++ kalunba/src/Components/Footer.jsx
@@ -1,30 +1,30 @@
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer className="footer py-4">
             <div className="container">
-               <div className="row align-items-center">
-                  <div className="col-lg-4 text-lg-left">
-                     Copyright © Your Website 2020
-                  </div>
-                  <div className="col-lg-4 my-3 my-lg-0">
-                     <a className="btn btn-dark btn-social mx-2" href="#!">
-                        <i className="fa fa-twitter"></i>
-                     </a>
-                     <a className="btn btn-dark btn-social mx-2" href="#!">
-                        <i className="fa fa-facebook-f"></i>
-                     </a>
-                     <a className="btn btn-dark btn-social mx-2" href="#!">
-                        <i className="fa fa-linkedin"></i>
-                     </a>
-                  </div>
-                  <div className="col-lg-4 text-lg-right">
-                     <a className="mr-3" href="#!">
-                        Privacy Policy
-                     </a>
-                     <a href="#!">Terms of Use</a>
-                  </div>
-               </div>
+                <div className="row align-items-center">
+                    <div className="col-lg-4 text-lg-left">
+                        Copyright © Your Website 2021
+                    </div>
+                    <div className="col-lg-4 my-3 my-lg-0">
+                        <a className="btn btn-dark btn-social mx-2" href="#!">
+                            <i className="fa fa-twitter"></i>
+                        </a>
+                        <a className="btn btn-dark btn-social mx-2" href="#!">
+                            <i className="fa fa-facebook-f"></i>
+                        </a>
+                        <a className="btn btn-dark btn-social mx-2" href="#!">
+                            <i className="fa fa-linkedin"></i>
+                        </a>
+                    </div>
+                    <div className="col-lg-4 text-lg-right">
+                        <a className="mr-3" href="#!">
+                            Privacy Policy
+                        </a>
+                        <a href="#!">Terms of Use</a>
+                    </div>
+                </div>
             </div>
-         </footer>
-    )
+        </footer>
+    );
 }
