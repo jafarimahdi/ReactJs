8e32158e8300e0279480df3b5d732bd2121f1632
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-07-03 08:06:00 +0200

    refactoring

--- kalunba/src/Components/Project.jsx
+++ kalunba/src/Components/Project.jsx
@@ -1,48 +1,44 @@
 export default function Project() {
-    const portLinks = [
+    const outDoor = [
         {
-            title: "Threads",
-            caption: "Illustration",
+            title: "OutDoor Programing",
+            caption: "write more about this card",
             pic: "pictures/kalunba/1.jpg",
         },
         {
+            title: "Lines",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/27.jpg",
+        },
+        {
+            title: "Lines",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/26.jpg",
+        },
+        {
             title: "Explore",
-            caption: "Graphic Design",
+            caption: "write more about this card",
             pic: "pictures/kalunba/8.jpg",
         },
         {
             title: "Finish",
-            caption: "Identity",
+            caption: "write more about this card",
             pic: "pictures/kalunba/9.jpg",
         },
         {
-            title: "Southwest",
-            caption: "Website Design",
-            pic: "pictures/kalunba/7.jpg",
-        },
-        {
-            title: "Windows",
-            caption: "Photography",
-            pic: "pictures/kalunba/5.jpg",
-        },
-        {
             title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/12.jpg",
         },
+
         {
             title: "Lines",
-            caption: "Branding",
-            pic: "pictures/kalunba/14.jpg",
-        },
-        {
-            title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/15.jpg",
         },
         {
             title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/16.jpg",
         },
         {
@@ -52,22 +48,30 @@ export default function Project() {
         },
         {
             title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/18.jpg",
         },
         {
             title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/19.jpg",
         },
         {
             title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/20.jpg",
         },
+    ];
+    const education = [
+        {
+            title: "Hungarian lesson",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/23.jpg",
+        },
+        
         {
             title: "Lines",
-            caption: "Branding",
+            caption: "write more about this card",
             pic: "pictures/kalunba/21.jpg",
         },
         {
@@ -77,36 +81,38 @@ export default function Project() {
         },
         {
             title: "Lines",
-            caption: "Branding",
-            pic: "pictures/kalunba/23.jpg",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/14.jpg",
         },
         {
-            title: "Lines",
-            caption: "Branding",
-            pic: "pictures/kalunba/24.jpg",
-        },
-        {
-            title: "Lines",
-            caption: "Branding",
+            title: "8 grade in Kalunba",
+            caption: "write more about this card",
             pic: "pictures/kalunba/25.jpg",
-        },
-        {
-            title: "Lines",
-            caption: "Branding",
-            pic: "pictures/kalunba/26.jpg",
-        },
-        {
-            title: "Lines",
-            caption: "Branding",
-            pic: "pictures/kalunba/27.jpg",
         },
     ];
 
+    const job = [
+        {
+            title: "job searching (BKV)",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/24.jpg",
+        },
+        {
+            title: "visit factory",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/5.jpg",
+        },
+        {
+            title: "prepping for the chrisms Market",
+            caption: "write more about this card",
+            pic: "pictures/kalunba/7.jpg",
+        },
+    ];
     return (
         <section className="page-section bg-light" id="project">
             <div className="container">
                 <div className="text-center">
-                    <h2 className="section-heading text-uppercase">Project</h2>
+                    <h2 className="section-heading text-uppercase">Projects</h2>
                     <h3 className="section-subheading text-muted">
                         Many of our Main project are for refugee kids and to
                         help them to find the comfort zone between home and new
@@ -114,49 +120,14 @@ export default function Project() {
                     </h3>
                 </div>
 
-                {/* <div className="row"> */}
-                <div className="card-deck">
-                    {portLinks.map((item) => (
+                {/* first Project of the list  */}
+                <div className="card-group ">
+                    {outDoor.map((item) => (
                         <div className="col-lg-4 col-sm-6 mb-4">
-                            {/* <div className="card-columns"> */}
                             <div
-                                className="card"
+                                className="card border border-lg"
                                 style={{ maxWidth: "21rem;" }}
                             >
-                                {/* <div className="portfolio-item">
-
-                                 <a
-                                    className="portfolio-link"
-                                    data-toggle="modal"
-                                    href="#portfolioModal1"
-                                >
-                                    <div className="portfolio-hover">
-                                        <div className="portfolio-hover-content">
-                                            <i className="fa fa-plus fa-3x" />
-                                        </div>
-                                    </div>
-
-                                    <img
-                                        className="card-image-top img-fluid"
-                                        src={item.pic}
-                                        alt=""
-                                    />
-                                </a>
-
-                                <div className="portfolio-caption">
-                                    <div className="portfolio-caption-heading">
-                                        {" "}
-                                        {item.title}{" "}
-                                    </div>
-                                    <div className="portfolio-caption-subheading text-muted">
-                                        {" "}
-                                        {item.caption}{" "}
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div> */}
-
                                 <img
                                     src={item.pic}
                                     className="card-img-top"
@@ -168,16 +139,105 @@ export default function Project() {
                                         position: "center",
                                     }}
                                 />
-                                <div className="card-body">
-                                    <h5 className="card-title">{item.title}</h5>
-                                    <p className="card-text">{item.caption}</p>
-                                    {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
-                                    {/* </div> */}
-                                </div>
+                                <section className="card-body">
+                                    <summery className="card-title">
+                                        {item.title}
+                                    </summery>
+
+                                    <details>
+                                        <p className="card-text">
+                                            {item.caption}
+                                        </p>
+                                        {/* <a href="#" class="btn btn-primary">
+                                            Go somewhere
+                                        </a> */}
+                                    </details>
+                                </section>
                             </div>
                         </div>
                     ))}
                 </div>
+                {/* new line ---------------------- */}
+                <h4 className=" m-5">Our Education program</h4>
+                <div className="card-group  p-2">
+                    {education.map((item) => (
+                        <div className="col-lg-4 col-sm-6 mb-4">
+                            <div
+                                className="card rounded "
+                                style={{ maxWidth: "21rem;" }}
+                            >
+                                <img
+                                    src={item.pic}
+                                    className="card-img-top"
+                                    alt="..."
+                                    style={{
+                                        width: "100%",
+                                        height: "33vh",
+                                        objectFit: "cover",
+                                        position: "center",
+                                    }}
+                                />
+                                <section className="card-body">
+                                    <summery className="card-title">
+                                        {item.title}
+                                    </summery>
+
+                                    <details>
+                                        <p className="card-text">
+                                            {item.caption}
+                                        </p>
+                                        <a href="#" class="btn btn-primary">
+                                            Go somewhere
+                                        </a>
+                                        {/* </div> */}
+                                    </details>
+                                </section>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                    {/* job searching part   */}
+
+                    <h4 className=" m-5">Job Market</h4>
+                <div className="card-group border border-secondary rounded-lg p-2">
+                    {job.map((item) => (
+                        <div className="col-lg-4 col-sm-6 mb-4">
+                            <div
+                                className="card rounded-lg "
+                                style={{ maxWidth: "21rem;" }}
+                            >
+                                <img
+                                    src={item.pic}
+                                    className="card-img-top"
+                                    alt="..."
+                                    style={{
+                                        width: "100%",
+                                        height: "33vh",
+                                        objectFit: "cover",
+                                        position: "center",
+                                    }}
+                                />
+                                <section className="card-body">
+                                    <summery className="card-title">
+                                        {item.title}
+                                    </summery>
+
+                                    <details>
+                                        <p className="card-text">
+                                            {item.caption}
+                                        </p>
+                                        <a href="#" class="btn btn-primary">
+                                            Go somewhere
+                                        </a>
+                                        {/* </div> */}
+                                    </details>
+                                </section>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+
             </div>
         </section>
     );
