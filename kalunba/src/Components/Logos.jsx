2bbd7834f265fae47dd3983443bc4d6695d1e694
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-07-06 08:56:50 +0200

    review Logos

--- kalunba/src/Components/Logos.jsx
+++ kalunba/src/Components/Logos.jsx
@@ -1,28 +1,22 @@
-
 export default function Logos() {
-   const logos = [
-      { pic: "pictures/kalunba/logo-rbg.png" },
-      { pic: "pictures/kalunba/scottishmission.jpg" },
-      { pic: "pictures/kalunba/reformatus.png" },
-   ];
-   return (
-      <div className="py-5">
-         <div className="container">
-            <div className="row">
-               {logos.map((item) => (
-                  <div className="col-md-3 col-sm-6 my-3">
-                     <a href="#!">
-                        <img
-                           className="img-fluid d-block mx-auto"
-                           src={item.pic}
-                           alt=""
-                           style={{ Width: "21rem;", height:"3vw;" }}
-                        />
-                     </a>
-                  </div>
-               ))}
+    const logos = [
+        { pic: "pictures/kalunba/logo-rbg.png" },
+        { pic: "pictures/kalunba/scottishmission.jpg" },
+        { pic: "pictures/kalunba/reformatus.png" },
+    ];
+    return (
+            <div className="container py-4 ">
+                <div className="row text-center">
+                    {logos.map((item) => (
+                        <div className="col-lg-4 col-md-6 col-sm-12 mx-auto my-auto p-3 ">
+                            <img
+                                className="mx-auto "
+                                src={item.pic}
+                                alt=""
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
-         </div>
-      </div>
-   );
+    );
 }
