a1f2811cd6eba1a1f9c8eab5194b010073540a1b
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-09-07 08:28:55 +0200

    refactoring footer

--- irancukrazda/src/component/Footer.js
+++ irancukrazda/src/component/Footer.js
@@ -4,40 +4,44 @@ import { Button } from "./Button";
 import "./Footer.scss";
 
 function Footer() {
-
-    const links=[
+    const links = [
         {
-            className:'', 
-            aria:'', 
-            icon:'',
+            className: "facebook",
+            aria: "facebook",
+            icon: "fa-facebook-f",
+            to: "/",
         },
         {
-            className:'', 
-            aria:'', 
-            icon:'',
+            className: "instagram",
+            aria: "Instagram",
+            icon: "fa-instagram",
+            to: "/",
         },
         {
-            className:'', 
-            aria:'', 
-            icon:'',
+            className: "youtube",
+            aria: "youtube",
+            icon: "fa-youtube",
+            to: "/",
         },
         {
-            className:'', 
-            aria:'', 
-            icon:'',
+            className: "twitter",
+            aria: "twitter",
+            icon: "fa-twitter",
+            to: "/",
         },
         {
-            className:'', 
-            aria:'', 
-            icon:'',
+            className: "linkedin",
+            aria: "linkedin",
+            icon: "fa-linkedin",
+            to: "/",
         },
-    ]
+    ];
     return (
         <div className="footer-container">
             <section className="footer-subscription">
                 <p className="footer-subscription-heading">
-                    Join the newsletter to receive our best 
-                    deals and new product!
+                    Join the newsletter to receive our best deals and new
+                    product!
                 </p>
                 <p className="footer-subscription-text">
                     You can unsubscribe any time
@@ -54,94 +58,28 @@ function Footer() {
                 </form>
             </section>
 
-            {/* <div className="footer-links">
-                <div className="footer-link-wrapper">
-                    <div className="footer-link-items">
-                        <h2>About Us</h2>
-                        <Link to="/sign-up">How it works</Link>
-                        <Link to="/">Testimonials</Link>
-                        <Link to="/">Careers</Link>
-                        <Link to="/">Investors</Link>
-                    </div>
-                    <div className="footer-link-items">
-                        <h2>Contact Us</h2>
-                        <Link to="/">Contact</Link>
-                        <Link to="/">Support</Link>
-                        <Link to="/">Destinations</Link>
-                        <Link to="/">Sponsorships</Link>
-                    </div>
-                </div>
-
-                <div className="footer-link-wrapper">
-                    <div className="footer-link-items">
-                        <h2>Video</h2>
-                        <Link to="/">Submit Video</Link>
-                        <Link to="/">Ambassadors</Link>
-                        <Link to="/">Agency</Link>
-                        <Link to="/">Influencer</Link>
-                    </div>
-
-                    <div className="footer-link-items">
-                        <h2>Social Media</h2>
-                        <Link to="/">Instagram</Link>
-                        <Link to="/">Facebook</Link>
-                        <Link to="/">Youtube</Link>
-                        <Link to="/">Twitter</Link>
-                    </div>
-                </div>
-            </div> */}
-
             <div className="social-media-wrap">
-
-                <a className="social-logo" href="https://jafarimahdi.github.io/" target="_blank">
+                <a
+                    className="social-logo"
+                    href="https://jafarimahdi.github.io/"
+                    target="_blank"
+                >
                     Made With
-                    <i class="fab fa-gratipay"/>
+                    <i class="fab fa-gratipay" />
                     In Apr 2021 By MJ.
                 </a>
 
-
                 <div className="social-icons">
-                    <Link
-                        className="social-icon-link facebook"
-                        to="/"
-                        target="_blank"
-                        aria-label="facebook"
-                    >
-                        <i className="fab fa-facebook-f" />
-                    </Link>
-
-                    <Link
-                        className="social-icon-link instagram"
-                        to="/"
-                        target="_blank"
-                        aria-label="Instagram"
-                    >
-                        <i className="fab fa-instagram" />
-                    </Link>
-                    <Link
-                        className="social-icon-link youtube"
-                        to="/"
-                        target="_blank"
-                        aria-label="youtube"
-                    >
-                        <i className="fab fa-youtube" />
-                    </Link>
-                    <Link
-                        className="social-icon-link twitter"
-                        to="/"
-                        target="_blank"
-                        aria-label="twitter"
-                    >
-                        <i className="fab fa-twitter" />
-                    </Link>
-                    <Link
-                        className="social-icon-link linkedin"
-                        to="/"
-                        target="_blank"
-                        aria-label="linkedin"
-                    >
-                        <i className="fab fa-linkedin" />
-                    </Link>
+                    {links.map((item) => (
+                        <Link
+                            className={`social-icon-link ${item.className}`}
+                            to={item.to}
+                            target="_blank"
+                            aria-label={item.label}
+                        >
+                            <i className={`fab ${item.icon}`} />
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>
